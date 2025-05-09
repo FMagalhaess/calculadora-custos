1440c483b2925e7e7cceae9476ec5d5d140e819f
FMagalhaess <7filipe093@gmail.com>
2025-05-09 14:26:41 -0300

    Exclusão total de arquivos obsoletos da logica antiga

--- DTO/PreparationDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace calculadora_custos.DTO;
-
-public class PreparationCostsReturnedByRecipeDTO
-{
-    public int Id { get; set; }
-    public string? RecipeName { get; set; }
-    public string? PreparationCostName { get; set; }
-}
--- DTO/PresentationCostDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace calculadora_custos.DTO;
-
-public class PresentationCostsReturnedByRecipeDTO
-{
-    public int Id { get; set; }
-    public string? RecipeName { get; set; }
-    public string? PresentationCostName { get; set; }
-}
